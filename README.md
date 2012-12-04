58a41238032e82744bd94c424171f8c11cff3d68
Carlo DiCelico <carlodicelico@gmail.com>
2012-12-04 09:53:24 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+NameGame
+========
+
+A simple NodeJS script that generates startup names and checks to see if the corresponding domain name is available.
+
+Requirements
+------------
+
+NameGame requires node 0.8.x and the colors, fs, http, commander, and sugar node modules.
+
+Usage
+-----
+
+To use NameGame from the command-line, update namegame.js to use your DNS API provider and API key and do `node namegame.js`.
+
+To use the web interface, just open namegame.html in your browser. Please note that domain-name checking is only supported in the command-line.
+
+Contributing
+------------
+
+Pull requests are welcome and feel free to fork!
+
+
---
+++ namegame.js
@@ -0,0 +1,116 @@
+// Module dependencies
+var colors = require('colors')
+    , fs = require('fs')
+    , http = require('http')
+    , program = require('commander')
+    , sugar = require('sugar');
+
+// Prefixes
+var prefixes = [
+    'be'
+    , 'do'
+    , 're'
+];
+
+// Words
+var words = [
+    'deed'
+    , 'help'
+    , 'friend'
+    , 'quest'
+    , 'love'
+    , 'true'
+    , 'play'
+    , 'game'
+    , 'fun'
+    , 'good'
+    , 'buddy'
+    , 'happy'
+    , 'boost'
+];
+
+// Suffixes
+var suffixes = [
+    'str'
+    , 'ify'
+    , 'fly'
+    , 'spot'
+    , 'r'
+    , 'ly'
+    , 'space'
+    , 'place'
+    , 'ful'
+    , 'quest'
+    , 'zy'
+    , 'tsy'
+    , 'sauce'
+    , 'boost'
+];
+
+var names = new Array();
+
+// Just the words
+words.each(function(word) {
+    names.push(word);
+});
+
+// Each word with a prefix
+words.each(function(word) {
+    prefixes.each(function(prefix) {
+        names.push(prefix + word);
+    });
+});
+
+// Each word with a suffix
+words.each(function(word) {
+    suffixes.each(function(suffix){
+        names.push(word + suffix);
+    });
+});
+
+// Each word with both a prefix and a suffix
+words.each(function(word) {
+    suffixes.each(function(suffix) {
+        prefixes.each(function(prefix) {
+            names.push(prefix + word + suffix);
+        });
+    });
+});
+
+var startups = names.sortBy();
+var len = startups.count();
+var msg = '\nGenerated ' + len + ' startup names! Checking to see which ones are available...\n';
+var log = fs.createWriteStream(new Date().getTime().toString() + '.log', {'flags': 'a'});
+//var counter = 0;
+
+console.log(msg.bold.yellow);
+
+startups.each(function(startup) {
+    // Set up your DNS API provider's options here. I use RoboWhoIs but you should be able to use any API you prefer.
+    var options = {
+          host: 'api.robowhois.com'
+        , method: 'GET'
+        , path: '/whois/' + startup + '.com/availability'
+        , auth: 'YOUR API KEY HERE'
+    };
+
+    http.request(options, function(response) {
+        var domainData = '';
+
+        response.on('data', function(chunk) {
+            domainData += chunk;
+        });
+
+        response.on('end', function() {
+            var obj = JSON.parse(domainData);
+            if(obj.error) {
+                console.log('ERROR!'.bold.red.inverse + ' ' + obj.error.name + ' while checking ' + startup);
+                log.write('ERROR!\n' + options.path.toString() + '\n' + obj.error.name + ' while checking ' + startup + '\nEND ERROR\n\n');
+            } else if(obj.response.available && !obj.response.registered) {
+               console.log(startup.bold.green.inverse);
+               log.write('SUCCESS!' + options.path.toString() + '\n' + domainData + '\n\n');
+               //counter += 1;
+            }
+        });
+    }).end();
+});
